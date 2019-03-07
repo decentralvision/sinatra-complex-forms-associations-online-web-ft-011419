65a6dae242d323e41d9f8fa69a571cad81e2add3
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-07 16:38:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,8 +11,9 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet])
     binding.pry
+    @pet = Pet.create(params[:pet])
+
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
